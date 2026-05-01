f80084ad345208c7999a00feb43b235e5982dd12
sundalei <sundalei1988@gmail.com>
2026-05-01 12:58:02 +0800

    refactor(elasticsearch): restructure elasticsearch configuration and client setup
    - Replace custom SSL handling with Spring's RestClientSsl for better security
    - Update configuration properties to use new elasticsearch.* prefix
    - Simplify RestClient creation using Spring's auto-configured builder
    - Remove unused secret properties and clean up configuration files

--- mongo-es-demo/src/main/java/com/example/config/ElasticConfig.java
+++ mongo-es-demo/src/main/java/com/example/config/ElasticConfig.java
@@ -1,62 +1,35 @@
 package com.example.config;
 
-import java.net.http.HttpClient;
-import java.security.SecureRandom;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
 import java.util.Base64;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.restclient.autoconfigure.RestClientSsl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.JdkClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class ElasticConfig {
 
-  @Value("${spring.elasticsearch.username}")
+  @Value("${elasticsearch.base-url}")
+  private String elasticsearchBaseUrl;
+
+  @Value("${elasticsearch.username}")
   private String username;
 
-  @Value("${spring.elasticsearch.password}")
+  @Value("${elasticsearch.password}")
   private String password;
 
   @Bean
-  public RestClient restClient() throws Exception {
-
-    TrustManager[] trustAllCerts =
-        new TrustManager[] {
-          new X509TrustManager() {
-            @Override
-            public void checkClientTrusted(X509Certificate[] chain, String authType)
-                throws CertificateException {}
-
-            @Override
-            public void checkServerTrusted(X509Certificate[] chain, String authType)
-                throws CertificateException {}
-
-            @Override
-            public X509Certificate[] getAcceptedIssuers() {
-              return null;
-            }
-          }
-        };
-
-    SSLContext sslContext = SSLContext.getInstance("TLS");
-    sslContext.init(null, trustAllCerts, new SecureRandom());
-
-    HttpClient httpClient = HttpClient.newBuilder().sslContext(sslContext).build();
-    ClientHttpRequestFactory requestFactory = new JdkClientHttpRequestFactory(httpClient);
-    String authHeader =
-        "Basic " + Base64.getEncoder().encodeToString((username + ":" + password).getBytes());
-
-    return RestClient.builder()
-        .baseUrl("https://75.127.15.212:9200") // Point to your Master node
-        .defaultHeader("Authorization", authHeader)
-        .requestFactory(requestFactory)
+  public RestClient elasticsearchRestClient(
+      RestClient.Builder restClientBuilder, RestClientSsl ssl) {
+    return restClientBuilder
+        .baseUrl(elasticsearchBaseUrl)
+        .defaultHeader("Authorization", createAuthHeader())
+        .apply(ssl.fromBundle("elastic-client"))
         .build();
+  }
+
+  private String createAuthHeader() {
+    return "Basic " + Base64.getEncoder().encodeToString((username + ":" + password).getBytes());
   }
 }
