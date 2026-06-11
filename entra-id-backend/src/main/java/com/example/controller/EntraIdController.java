3021430ee3f1ebbf6566a8a115a491bb5d2018cf
sundalei <sundalei1988@gmail.com>
2026-06-11 22:53:34 +0800

    chore(controller): remove unused import statement

--- entra-id-backend/src/main/java/com/example/controller/EntraIdController.java
+++ entra-id-backend/src/main/java/com/example/controller/EntraIdController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import tools.jackson.core.type.TypeReference;
 import tools.jackson.databind.ObjectMapper;
 
